893f3aae40b4e6fdfc3a1df4671f62ce53613723
yjclarelee <yjclarelee@postech.ac.kr>
2022-03-19 12:38:18 +0900

    Add strikethrough functionality

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Main() {
-  const [todoItem, setTodoItem] = React.useState('');
+  const [todoItem, setTodoItem] = React.useState({todo: '', isStrike: false});
   const [todoList, setTodoList] = React.useState([]);
   // string is null -> do nothing
   // string is in array -> setTodo to ''
@@ -11,32 +11,40 @@ export default function Main() {
     // input when todoItem exists
     if (todoItem) {
       // prevent same item
+      // FIXME: not working
       if (!todoList.includes(todoItem)) {
         setTodoList([...todoList, todoItem]);
       }
-        setTodoItem('');
+        setTodoItem({});
     }
   };
 
-  const listItem = (todo, idx) => {
-    return (
-      <div key={`list-${idx}`} style={{ display: 'flex' }}>
-        <li key={`text-${idx}`}>{todo}</li>
-        <button key={`deleteButton-${idx}`} onClick={() => setTodoList([...todoList.filter(item => item !== todo)])}>X</button>
-      </div>
-    )
+  const handleStrike = (idx) => {
+    setTodoList([...todoList.map((item, itemIdx) =>
+      itemIdx !== idx ? item : { todo: todoList[idx].todo, isStrike: !todoList[idx].isStrike }
+    )])
   }
 
+  const listItem = (idx) => {
+    console.log(todoList);
+    return (
+      <div key={`list-${idx}`} style={{ display: 'flex' }}>
+        <button onClick={() => handleStrike(idx)}>V</button>
+        <li key={idx}>{todoList[idx].isStrike ? <del>{todoList[idx].todo}</del> : todoList[idx].todo}</li>
+        <button onClick={() => setTodoList([...todoList.filter(item => item.todo !== todoList[idx].todo)])}>X</button>
+      </div>  
+    )
+  }
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <Header />
       <div>
-        <input type="text" value={todoItem} onChange={(e) => setTodoItem(e.target.value)} onKeyDown={(e) => {e.key === 'Enter' && handleInput()}}/>
+        <input type="text" value={todoItem.todo || ''} onChange={(e) => setTodoItem({todo: e.target.value, isStrike: false})} onKeyDown={(e) => {e.key === 'Enter' && handleInput()}}/>
         <button type="button" onClick={handleInput}>Add</button>
       </div>
-      <ul>
-         {(todoList.length !== 0) && todoList.map((todo, idx) => listItem(todo, idx))}
+      <ul style={{listStyleType: 'none'}}>
+         {(todoList.length !== 0) && todoList.map((item, idx) => listItem(idx))}
       </ul>
       <Footer/>
     </div> 
